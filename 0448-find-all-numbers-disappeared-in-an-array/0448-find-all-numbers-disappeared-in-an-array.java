8226e33e7a53c7627bb7a27c1871313ef96fe752
ayushie18 <ayushi18parashar@gmail.com>
2025-11-14 02:17:49 +0530

    Time: 29 ms (5.07%), Space: 74.4 MB (5.08%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,18 +1,33 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int n=nums.length;
-        boolean [] present=new boolean[n+1];
-        for(int i:nums){
-            if(i>=1 && i<=n) present[i]=true;
+         int n=nums.length;
+        // boolean [] present=new boolean[n+1];
+        // for(int i:nums){
+        //     if(i>=1 && i<=n) present[i]=true;
+        // }
+         List<Integer>ans=new ArrayList<>();
+        // for(int i=1;i<=n;i++){
+        //     if(!present[i]){
+        //        ans.add(i);
+        //     }
+        //  }
+        // return ans;
+      HashMap<Integer,Integer> map=new HashMap<>();
+      for(int i:nums){
+        if(map.containsKey(i)){
+            int freq=map.get(i);
+            map.put(i,freq+1);
         }
-        List<Integer>ans=new ArrayList<>();
-        for(int i=1;i<=n;i++){
-            if(!present[i]){
-               ans.add(i);
-            }
+        else map.put(i,1);
+    
+      }
+      for(int i=1;i<=n;i++){
+          if(!map.containsKey(i)){
+            ans.add(i);
+       }
 
-        }
-        return ans;
+      }
 
+    return ans;
     }
 }
