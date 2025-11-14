757b4861c63e543cfa1a39dcb8223dfd94747929
ayushie18 <ayushi18parashar@gmail.com>
2025-11-14 20:51:50 +0530

    Time: 7 ms (8.59%), Space: 47.2 MB (16.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,38 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-      int count=0;
-      int n=nums.length;
-      for(int i=0; i<n;i++){
-        if(nums[i]!=0){
-            nums[count]=nums[i];
-            count++;
-        }
-      }
+        int n=nums.length;
+    //   int nonZeroes=0;
+    //   for(int i=0; i<n;i++){
+    //     if(nums[i]!=0){
+    //         nums[nonZeroes]=nums[i];
+    //         nonZeroes++;
+    //     }
+    //   }
+    //   while(nonZeroes<n){
+    //     nums[nonZeroes]=0;
+    //     nonZeroes++;
+    //   }
 
-      while(count<n){
-        nums[count]=0;
-        count++;
-      }
+    Stack<Integer> st=new Stack<>();
+    int cnt=0;
+    
+    for(int i:nums){
+      if(i!=0){
+        st.push(i);
+        cnt++;
 
-        
+      }
     }
+    int j=cnt-1;
+     while(!st.isEmpty()){
+         nums[j--]=st.pop();
+     }
+    
+ 
+   for(int c=cnt;c<n;c++){
+    nums[c]=0;
+   }
+
+    
+  }
 }
