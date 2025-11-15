ad2b667a939da84020f0d839287f974554bb0dfa
ayushie18 <ayushi18parashar@gmail.com>
2025-11-15 19:52:46 +0530

    Time: 1 ms (100%), Space: 47 MB (70.19%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,20 +1,35 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n=nums.length;
-        //int[] ans=new int[n];
-        for(int i=0;i<n;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>nums[j]){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
+        int []ans=new int[n];
+        int i=0;
+        int j=n-1;
+        int k=n-1;
+        while(i<=j){
+            if(nums[i]*nums[i]>nums[j]*nums[j]){
+                ans[k]=nums[i]*nums[i];
+               i++;
             }
+            else {
+            ans[k]=nums[j]*nums[j];
+             j--;
+            }
+            k--;
         }
-        return nums;
+        
+        // for(int i=0;i<n;i++){
+        //     nums[i]=nums[i]*nums[i];
+        // }
+        // for(int i=0;i<n;i++){// Arrays.sort(nums)
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]>nums[j]){
+        //             int temp=nums[i];
+        //             nums[i]=nums[j];
+        //             nums[j]=temp;
+        //         }
+        //     }
+        // }
+        return ans;
         
     }
 }
