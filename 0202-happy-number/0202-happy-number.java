d11818db90d20a8ef15f87943d5220e029fb4852
ayushie18 <ayushi18parashar@gmail.com>
2025-11-19 20:10:33 +0530

    Time: 1 ms (70.31%), Space: 42.1 MB (24.66%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int squareOfDigit(int num){
+        int sum=0;
+        while(num>0){
+            int dig=num%10;
+            sum+=dig*dig;
+            num/=10;
+        }
+        return sum;
+    }
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        while(fast!=1){
+            slow=squareOfDigit(slow);
+            fast=squareOfDigit(squareOfDigit(fast));
+
+        if(fast==1) return true;
+        if(slow==fast) return false;
+        }
+        return true;
+    }
+}
