33834276fa48230557fd3f65dc8049a38de63a69
ayushie18 <ayushi18parashar@gmail.com>
2025-11-28 01:32:56 +0530

    Time: 0 ms (100%), Space: 42 MB (17.16%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,7 +1,13 @@
 class Solution {
     public int gcd(int a, int b){
-     if(a%b==0) return b;
-      return gcd(b,a%b);
+    //  if(a%b==0) return b;
+    //   return gcd(b,a%b);
+    while(b!=0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
     }
     public int lcm(int a,int b){
         return a*b/gcd(a,b);
