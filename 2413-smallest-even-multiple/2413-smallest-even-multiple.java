7c269a0426fd25a0482069ad734ee86b4d4a5c01
ayushie18 <ayushi18parashar@gmail.com>
2025-11-28 01:25:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (10.29%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int gcd(int a, int b){
+     if(a%b==0) return b;
+      return gcd(b,a%b);
+    }
+    public int lcm(int a,int b){
+        return a*b/gcd(a,b);
+    }
+    public int smallestEvenMultiple(int n) {
+        return lcm(2,n);
+    }
+}
