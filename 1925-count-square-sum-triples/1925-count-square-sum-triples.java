b9b71866e3b85d925084dc03f9e0d1ea02e70702
ayushie18 <ayushi18parashar@gmail.com>
2025-12-08 16:04:55 +0530

    Time: 13 ms (56.16%), Space: 42 MB (49.69%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -3,10 +3,9 @@ class Solution {
         int count=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                for(int k=1;k<=n;k++){
-                    if(i*i+j*j==k*k) count++;
-              
-                }
+               int third=i*i+j*j;
+               int sqrthird=(int)Math.sqrt(third);
+               if(sqrthird<=n && sqrthird*sqrthird==third) count++;
             }
         }
     return count;
