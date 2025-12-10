86311cb4e4f59641f768fb52635f1f35a23851ed
ayushie18 <ayushi18parashar@gmail.com>
2025-12-10 12:09:14 +0530

    Time: 225 ms (64.57%), Space: 192.6 MB (19.69%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.java
+++ 3583-count-special-triplets/3583-count-special-triplets.java
@@ -1,29 +1,44 @@
 class Solution {
     public int specialTriplets(int[] nums) {
-        long result=0;
-        int n=nums.length;
-        long mod=1000000007;
-        HashMap<Integer,Integer> leftmap=new HashMap<>();
-        HashMap<Integer,Integer> rightmap=new HashMap<>();
-        for(int i=0;i<n;i++){
-           rightmap.put(nums[i],rightmap.getOrDefault(nums[i],0)+1);
-        }
-        for(int ele:nums){
-            if (rightmap.containsKey(ele)) {
-            rightmap.put(ele, rightmap.get(ele) - 1);
-            if (rightmap.get(ele) == 0) {
-             rightmap.remove(ele);
+        // TWO PASS SOLUTION
+         long result=0;
+         int n=nums.length;
+         long mod=1000000007;
+        // HashMap<Integer,Integer> leftmap=new HashMap<>();
+        // HashMap<Integer,Integer> rightmap=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //    rightmap.put(nums[i],rightmap.getOrDefault(nums[i],0)+1);
+        // }
+        // for(int ele:nums){
+        //     if (rightmap.containsKey(ele)) {
+        //     rightmap.put(ele, rightmap.get(ele) - 1);}
+        //     if (rightmap.get(ele) == 0) {
+        //      rightmap.remove(ele);
+        //     }
+        
+        //     int target=ele*2;
+        //     int left=leftmap.getOrDefault(target,0);
+        //     int right=rightmap.getOrDefault(target,0);
+
+        //     result=result+(1L*left*right)%mod;
+        //     leftmap.put(ele,leftmap.getOrDefault(ele,0)+1);
+        // }
+
+        // return (int)(result%mod);
+        //ONE PASS SOLUTION
+
+        HashMap<Integer,Long> validI=new HashMap<>();
+        HashMap<Integer,Long> validJ=new HashMap<>();
+        for(int num:nums){
+            if(num%2==0){
+              result=(result+validJ.getOrDefault(num/2,0L))%mod;
             }
-}
-            int target=ele*2;
-            int left=leftmap.getOrDefault(target,0);
-            int right=rightmap.getOrDefault(target,0);
 
-            result=(result+(1L*left*right))%mod;
-            leftmap.put(ele,leftmap.getOrDefault(ele,0)+1);
+            long cnt=(validJ.getOrDefault(num,0L)+validI.getOrDefault(2*num,0L))%mod;
+            validJ.put(num,cnt);
+            validI.put(num,validI.getOrDefault(num,0L)+1);
         }
-
-        return (int)(result%mod);
+        return (int)result;
         
     }
 }
