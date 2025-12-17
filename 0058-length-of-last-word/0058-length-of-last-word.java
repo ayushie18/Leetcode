623bbc2cfdb27ec018ab5bdeb5538d28a89dea3e
ayushie18 <ayushi18parashar@gmail.com>
2025-12-17 23:27:02 +0530

    Time: 0 ms (100%), Space: 42 MB (79.73%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,29 @@
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//      int i=s.length()-1;
+//      int length =0;
+//      while(i>=0 & s.charAt(i)==' '){
+//         i--;
+
+//      }
+//      while(i>=0 && s.charAt(i)!=' '){
+//         length++;
+//         i--;
+//     }
+//  return length;
+// }
+// }
+class Solution{
+    public int lengthOfLastWord(String s){
+        int length=0;
+        boolean flag=false;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' ')
+            { length++;
+            flag=true;}
+            else if(flag)
+            break;
+        }
+        return length;
+    }
+}
