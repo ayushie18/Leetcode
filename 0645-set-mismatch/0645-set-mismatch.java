86398042852ab898073a51bff5e9499cd321a797
ayushie18 <ayushi18parashar@gmail.com>
2025-12-30 12:26:39 +0530

    Time: 1 ms (99.53%), Space: 48 MB (19.75%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n=nums.length;
+        int[] freq=new int[n+1];
+        int[] ans=new int[2];
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==2){
+                ans[0]=i;
+            }
+            else if( freq[i]==0){
+                ans[1]=i;
+            }
+        }
+        return ans;
+        
+    }
+}
