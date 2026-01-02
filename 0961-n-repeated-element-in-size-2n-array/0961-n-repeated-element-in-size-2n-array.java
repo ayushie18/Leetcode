1b806d0afb02465bd76d314f31580be4838c01fe
ayushie18 <ayushi18parashar@gmail.com>
2026-01-03 00:20:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -2,15 +2,22 @@ class Solution {
     public int repeatedNTimes(int[] nums) {
         int len=nums.length;
         int n=len/2;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int key: map.keySet()){
-            int freq=map.get(key);
-            if(freq==n) return key;
-        }
+    //     HashMap<Integer,Integer> map=new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    //     }
+    //     for(int key: map.keySet()){
+    //         int freq=map.get(key);
+    //         if(freq==n) return key;
+    //     }
         
-       return 0; 
+    //    return 0; 
+    for(int i=0;i<len-1;i++){
+        if(nums[i]==nums[i+1]|| nums[i]==nums[i+2]){
+            return nums[i];
+        }
+    }
+    return nums[n-1];
+     
     }
 }
