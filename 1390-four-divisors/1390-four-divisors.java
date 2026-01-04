32a7ba42142a4bf70508efe95f8d1b7fbfe9c44d
ayushie18 <ayushi18parashar@gmail.com>
2026-01-04 22:38:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int fourdivisors(int n){
+        int count=2;
+        int sum=1+n;
+        for(int i=2;i*i<n;i++){
+            if(n%i==0 ){ 
+             int div=n/i;
+             if(div==i){
+                count++;
+                sum+=i;
+             }
+             else{
+             count+=2;
+             sum+=i+div;
+             }
+            }
+        }
+        if(count==4) return sum;
+        return 0;
+    }
+    public int sumFourDivisors(int[] nums) {
+        int n=nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++){
+           ans+=fourdivisors(nums[i]);
+        }
+        return ans;
+    }
+}
