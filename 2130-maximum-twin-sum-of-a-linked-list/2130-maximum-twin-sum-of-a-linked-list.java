2bc43515d26375fe632d8b705d67eb8f3d84c4c5
ayushie18 <ayushi18parashar@gmail.com>
2026-01-06 13:07:06 +0530

    Time: 6 ms (36.07%), Space: 101 MB (84.2%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -9,25 +9,52 @@
  * }
  */
 class Solution {
+    public ListNode reverseLL(ListNode head){
+        ListNode temp=head;
+        ListNode prev=null;
+        while(temp!=null){
+            ListNode front=temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=front;
+        }
+        return prev;
+
+    }
     public int pairSum(ListNode head) {
-        ArrayList<Integer> list=new ArrayList<>();
-        while(head!=null){
-            list.add(head.val);
+        // ArrayList<Integer> list=new ArrayList<>();
+        // while(head!=null){
+        //     list.add(head.val);
+        //     head=head.next;
+        // }
+
+        // int max=Integer.MIN_VALUE;
+        // int i=0;
+        // int j=list.size()-1;
+        
+        // while(i<j){
+        //    int sum=list.get(i)+list.get(j);
+        //    max=Math.max(sum,max);
+        //    i++;
+        //    j--;
+
+            
+        // }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode temp=reverseLL(slow);
+        int max=Integer.MIN_VALUE;
+        while(temp!=null){
+            int sum=head.val+temp.val;
+            max=Math.max(sum,max);
+            temp=temp.next;
             head=head.next;
         }
 
-        int max=Integer.MIN_VALUE;
-        int i=0;
-        int j=list.size()-1;
-        
-        while(i<j){
-           int sum=list.get(i)+list.get(j);
-           max=Math.max(sum,max);
-           i++;
-           j--;
-
-            
-        }
         return max;
         
     }
