a126fb31c7e3aa9748a543f935eb1526c6c5a0b4
ayushie18 <ayushi18parashar@gmail.com>
2026-01-09 14:21:54 +0530

    Time: 83 ms (5.05%), Space: 104.4 MB (8.99%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -24,10 +24,11 @@ class Solution {
         int dup=0;
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<k;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],0);
-            }
-            map.put(nums[i],map.get(nums[i])+1);
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            // if(!map.containsKey(nums[i])){
+            //     map.put(nums[i],0);
+            // }
+            // map.put(nums[i],map.get(nums[i])+1);
             sum=sum+nums[i];
             if(map.get(nums[i])>1){
                 dup+=1;
