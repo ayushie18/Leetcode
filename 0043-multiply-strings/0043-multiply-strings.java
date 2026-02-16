63fe6d14ac57b1f730289e020756a03d830d94af
ayushie18 <ayushi18parashar@gmail.com>
2026-02-16 08:14:07 +0530

    Time: 3 ms (80.88%), Space: 43.4 MB (91.92%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,9 +1,32 @@
-import java.math.BigInteger;
+// import java.math.BigInteger; NOT ALLOWED
+// class Solution {
+//     public String multiply(String num1, String num2) {
+//         BigInteger a=new BigInteger(num1);
+//         BigInteger b=new BigInteger(num2);
+//         return a.multiply(b).toString();
+        
+//     }
+// }
 class Solution {
     public String multiply(String num1, String num2) {
-        BigInteger a=new BigInteger(num1);
-        BigInteger b=new BigInteger(num2);
-        return a.multiply(b).toString();
-        
+        if (num1.equals("0") || num2.equals("0"))
+            return "0";
+        int ans[]=new int[num1.length()+num2.length()];
+        int n=num1.length()-1, m=num2.length()-1;
+        for(int i=n;i>=0;i--){
+        for(int j=m;j>=0;j--){
+             int mul=(num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+             int sum=ans[i+j+1]+mul;
+             ans[i+j+1]=sum%10;
+             ans[i+j]+=sum/10;
+        }
+       }
+      StringBuilder sb=new StringBuilder();
+      for(int num:ans){
+        if(!(sb.length()==0 && num==0)){
+        sb.append(num);
+        }
+      }
+    return sb.toString();
     }
 }
