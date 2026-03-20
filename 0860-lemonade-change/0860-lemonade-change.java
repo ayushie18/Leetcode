89993c7f202701113e1c03abf7811ede5453065b
ayushie18 <ayushi18parashar@gmail.com>
2026-03-20 23:44:30 +0530

    Time: 2 ms (99.47%), Space: 72.4 MB (81.94%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        int twenty=0;
+    for(int i=0;i<bills.length;i++){
+        if(bills[i]==5) {
+            five++;
+        } 
+        else if(bills[i]==10) {
+            if(five>0){
+            ten++;
+            five--;
+             }
+            else {
+                return false;
+            }
+        }
+        else {
+            if(five>0 && ten>0){
+                five--;
+                ten--;
+                twenty++;
+            }
+            
+            else if(five>=3){
+                five-=3;
+                twenty++;
+            }
+            else{
+                return false;
+            }
+
+        }
+    }
+        return true;
+
+    }
+}
