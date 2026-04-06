ef909e24eb6bd8678a427f8a75c6b870999ce054
ayushie18 <ayushi18parashar@gmail.com>
2026-04-06 08:10:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character,Character> map1=new HashMap<>();
-        HashMap<Character,Character> map2=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-         char c1=s.charAt(i);
-         char c2=t.charAt(i);
-         if(map1.containsKey(c1)){
-            if(map1.get(c1)!=c2) return false;
-         }
-         else{
-            map1.put(c1,c2);
-         }
-         
-          if(map2.containsKey(c2)){
-            if(map2.get(c2)!=c1) return false;
-         }
-         else{
-            map2.put(c2,c1);
-         }
-       
-        }
+      char [] a=new char[128];
+      char [] b=new char [128];
+  for(int i=0;i<s.length();i++){
+    char first= s.charAt(i);
+    char second=t.charAt(i);
+    int idx=(int)first;
+
+    if(a[idx]=='\0') a[idx]=second;
+    else{
+         if(a[idx]!=second) return false;
+    }
+  }
+   for(int i=0;i<s.length();i++){
+    char first= t.charAt(i);
+    char second=s.charAt(i);
+    int idx=(int)first;
+
+    if(b[idx]=='\0') b[idx]=second;
+    else{
+         if(b[idx]!=second) return false;
+    }
+  }
+
+  
     return true;
  }
 }
