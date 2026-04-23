edcf5e245f290ceaaef788d8d37f86c26a756bb5
ayushie18 <ayushi18parashar@gmail.com>
2026-04-23 20:30:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,22 @@
 class Solution {
     public double myPow(double x, int n) {
-
-        return Math.pow(x,n);
-        
+        double ans=1;
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        while(n>0){
+            if(n%2==1){
+                ans=ans*x;
+                n=n-1;
+            }
+            else{
+                n=n/2;
+                x=x*x;
+            }
+        }
+         
+        return ans;
+    
     }
 }
