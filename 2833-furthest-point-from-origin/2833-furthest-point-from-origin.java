3009fcfa5cf84ac3da71b0e126328e2be14def09
ayushie18 <ayushi18parashar@gmail.com>
2026-04-24 11:21:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.java
@@ -3,26 +3,31 @@ class Solution {
         int n=moves.length();
         int LCount=0;
         int RCount=0;
+        int UCount=0;
         for(int i=0;i<n;i++){
             char c=moves.charAt(i);
             if(c=='L') LCount++;
-            if(c=='R') RCount++;
+            else if(c=='R') RCount++;
+            else UCount++;
         }
-         int dist=0;
-        for(int i=0;i<n;i++){
-        char c=moves.charAt(i); 
+    //      int dist=0;
+    //     for(int i=0;i<n;i++){
+    //     char c=moves.charAt(i); 
            
-           if(c=='R') dist+=1;
-           else if(c=='L') dist-=1;
-           else{
-           if(LCount>RCount){
-                dist-=1;
-            }
-            else{
-            dist+=1;
-           }
-        }
-    }    
-    return Math.abs(dist);
+    //        if(c=='R') dist+=1;
+    //        else if(c=='L') dist-=1;
+    //        else{
+    //        if(LCount>RCount){
+    //             dist-=1;
+    //         }
+    //         else{
+    //         dist+=1;
+    //        }
+    //     }
+    // }    
+    // return Math.abs(dist);
+     
+     return Math.abs(RCount-LCount)+UCount;
+
     }
 }
