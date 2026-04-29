b3323fc5010bc8b31405ed55d3a1549a57295951
ayushie18 <ayushi18parashar@gmail.com>
2026-04-29 12:03:54 +0530

    Time: 0 ms (100%), Space: 41.8 MB (84.9%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
-    // int [] dp=new int[38];
+    int [] dp=new int[38];
 
-    // public Solution(){
-    //    Arrays.fill(dp,-1);
-    // }
+    public Solution(){
+       Arrays.fill(dp,-1);
+    }
    
     
     public int tribonacci(int n) {
+        
         if(n==0) return 0;
         if(n==1||n==2) return 1;
-        int [] dp=new int[38];
-        Arrays.fill(dp,-1);
+      
         if(dp[n]!=-1) return dp[n];
         return dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
 
