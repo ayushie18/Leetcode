02ba43d1687618890c0341d23645e66da662f099
ayushie18 <ayushi18parashar@gmail.com>
2026-04-30 19:49:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,9 +1,9 @@
 class Solution {
 
   public int countPrimes(int n) {
-    if(n<=1) return 0;
+    if(n<=2) return 0;
 
-    boolean[] isPrime=new boolean[n+1];
+    boolean[] isPrime=new boolean[n];
     for(int i=0;i<n;i++){
         isPrime[i]=true;
     }
@@ -13,8 +13,8 @@ class Solution {
 
     for(int i=2;i*i<=n;i++){
         if(isPrime[i]==true){
-            for(int j=2;i*j<=n;j++){
-                isPrime[i*j]=false;
+            for(int j=i*i;j<n;j+=i){
+                isPrime[j]=false;
             }
         }
     }
