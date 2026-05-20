90aeaa169be61e0eb578a03ac5c752a8a5995ec7
ayushie18 <ayushi18parashar@gmail.com>
2026-05-21 03:30:34 +0530

    Time: 13 ms (99.85%), Space: 42.1 MB (52.58%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -5,18 +5,30 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int lo=1;
         int hi=n;
-        int ans=0;
-         while(lo<=hi){
+        // int ans=0;
+        //  while(lo<=hi){// TYPE 1 of BInary SeARCH
+        //     int mid=lo+(hi-lo)/2;
+        //      if(isBadVersion(mid)){
+        //         ans=mid;
+        //         hi=mid-1;
+        //      }
+        //      else{
+        //         lo=mid+1;
+        //      }
+        //  }
+        //  return ans;
+        while(lo<hi){
             int mid=lo+(hi-lo)/2;
-             if(isBadVersion(mid)){
-                ans=mid;
-                hi=mid-1;
-             }
-             else{
+            if(isBadVersion(mid)){
+                hi=mid;
+            }
+            else{
                 lo=mid+1;
-             }
-         }
-         return ans;
+            }
+        }
+        return lo;
+
+
         
     }
 }
