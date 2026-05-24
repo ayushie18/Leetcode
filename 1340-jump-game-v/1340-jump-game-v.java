6cedd0bd7b63783976333c40c43f95c52229cb7b
ayushie18 <ayushi18parashar@gmail.com>
2026-05-25 02:32:18 +0530

    Time: 10 ms (87.6%), Space: 46.6 MB (30.58%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxJumps(int[] arr, int d) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        int ans = 1;
+        for(int i=0;i<n;i++){
+            ans = Math.max(ans, dfs(i, arr, d, dp));
+        }
+     return ans;
+    }
+
+    public int dfs(int i, int[] arr, int d, int[] dp){
+
+        if(dp[i] != 0){
+            return dp[i];
+        }
+        int ans = 1;
+        // right
+        for(int j=i+1;j<=Math.min(arr.length-1, i+d);j++){
+
+            // blocked
+            if(arr[j] >= arr[i]){
+                break;
+            }
+            ans = Math.max(ans, 1 + dfs(j, arr, d, dp));
+        }
+
+        // left
+        for(int j=i-1;j>=Math.max(0, i-d);j--){
+
+            // blocked
+            if(arr[j] >= arr[i]){
+                break;
+            }
+
+            ans = Math.max(ans, 1 + dfs(j, arr, d, dp));
+        }
+
+        return dp[i] = ans;
+        
+    }
+}
