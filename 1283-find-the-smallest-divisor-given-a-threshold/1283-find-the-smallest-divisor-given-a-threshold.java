004508726afd44d9fed081cc8854d2ad2b158890
ayushie18 <ayushi18parashar@gmail.com>
2026-06-02 17:33:23 +0530

    Time: 18 ms (43.27%), Space: 50.5 MB (97.54%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -1,4 +1,17 @@
 class Solution {
+    public boolean isLess(int n,int[] nums, int k){
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%n==0){
+                sum+=nums[i]/n;
+            }
+            else{
+                sum+=nums[i]/n+1;
+            }
+        }
+        return sum<=k;
+
+    }
     public int smallestDivisor(int[] nums, int threshold) {
         int n=nums.length;
         int max=Integer.MAX_VALUE;
@@ -8,30 +21,26 @@ class Solution {
          int lo=1;
          int hi=max;
          int d=1;
-         while(lo<=hi){
+         while(lo<hi){
             int mid=lo+(hi-lo)/2;
-            int sum=0;
-            for(int i=0;i<n;i++){
-                if(nums[i]%mid==0) {
-                    sum+=nums[i]/mid;
-                }
-                else{
-                    sum+=nums[i]/mid+1;
+            // int sum=0;
+            // for(int i=0;i<n;i++){
+            //     if(nums[i]%mid==0) {
+            //         sum+=nums[i]/mid;
+            //     }
+            //     else{
+            //         sum+=nums[i]/mid+1;
 
-                }
+            //     }
+            // }
+            if(isLess(mid,nums,threshold)){
+                hi=mid;
             }
-                if(sum<=threshold){
-                    hi=mid-1;
-                    d=mid;
-
-                }
-                else{
-                    lo=mid+1;
-
-                }
-
+            else{
+                lo=mid+1;
             }
-    return d;
-        
+         }
+
+    return lo;
     }
 }
