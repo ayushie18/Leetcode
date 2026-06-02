ca1cf9451139986a7632213f7fca71bc5b9f8eaa
ayushie18 <ayushi18parashar@gmail.com>
2026-06-03 01:38:53 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31.02%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -14,18 +14,24 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length;
         int pivot=-1;
-        for(int i=nums.length-2;i>=0;i--){
+        for(int i=nums.length-2;i>=0;i--){// breaking point 
             if(nums[i]<nums[i+1]){
                 pivot=i;
                 break;
             }
         }
-        if(pivot==-1){
+        if(pivot==-1){// edge case like (3,2,1)--> reverse in asc order acc to ques
             reverse(nums,0,n-1);
             return;
         }
+        // Now we swap the pivot to next greater element
+        //(1 3 6 5 4)
+        //(1 4 6 5 3)-> after swapping
+        //      ..... still largest
+        // hence reverse from idx pivot+1 to end
+        //(1 4 3 5 6)-> next greater elemnt
 
-        for(int i=nums.length-1;i>pivot;i--){
+        for(int i=nums.length-1;i>pivot;i--){// swapping of pivot ele and next greater ele
             if(nums[i]>nums[pivot]){
                 int temp=nums[pivot];
                 nums[pivot]=nums[i];
@@ -34,7 +40,7 @@ class Solution {
             }
         }
 
-        reverse(nums,pivot+1,n-1);
+        reverse(nums,pivot+1,n-1);// reverse
 
 
     }
