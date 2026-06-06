ca0c3144c698cad372b5cb46e8f28a909e97e974
ayushie18 <ayushi18parashar@gmail.com>
2026-06-07 01:51:26 +0530

    Time: 3 ms (73.76%), Space: 52.5 MB (51.28%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,23 +1,55 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        // int [] freq=new int[nums.length];
+        // HashMap<Integer,Integer> map=new HashMap<>();
         // for(int i=0;i<nums.length;i++){
-        //     freq[num]
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         // }
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        List<Integer> ans=new ArrayList<>();
-        for(int ele:map.keySet()){
-            int freq=map.get(ele);
-            if(freq>nums.length/3){
-                ans.add(ele);
-            }
+        // List<Integer> ans=new ArrayList<>();
+        // for(int ele:map.keySet()){
+        //     int freq=map.get(ele);
+        //     if(freq>nums.length/3){
+        //         ans.add(ele);
+        //     }
 
-        }
-        return ans;
-
+        // }
+        // return ans;
+       int ans1=0;
+       int ans2=0;
+       int freq1=0;
+       int freq2=0;
+       for(int ele:nums){
+        if(ele==ans1) freq1++;
+        else if(ele==ans2) freq2++;
         
+        else if(freq1==0){
+            ans1=ele;
+            freq1++;
+        }
+        else if(freq2==0){
+            ans2=ele;
+            freq2++;
+
+        }
+    
+        else{
+            freq1--;
+            freq2--;
+        }
+       }
+       int count1=0;
+       int count2=0;
+       for(int ele:nums){
+        if(ele==ans1) count1++;
+        else if(ele==ans2) count2++;
+
+       }
+
+      List<Integer> ans=new ArrayList<>();
+      if(count1>nums.length/3) ans.add(ans1);
+      if (count2>nums.length/3) ans.add(ans2);
+       
+
+
+      return ans;  
     }
 }
