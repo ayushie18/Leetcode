7c9b506fd6b28fd3108280478fe7d8df4a4070f0
ayushie18 <ayushi18parashar@gmail.com>
2026-06-09 21:08:06 +0530

    Time: 1 ms (100%), Space: 71.1 MB (83.32%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -4,10 +4,10 @@ class Solution {
         int i=0;
         while(i<n){
             int correctIdx=nums[i]-1;
-            if(correctIdx<0 || correctIdx>=n){
+            if(correctIdx<0 || correctIdx>=n){// IMP because here we have -ve no. , 0 ...
                 i++;
             }
-            else if(nums[correctIdx]!=nums[i]){
+            else if(nums[i]!=nums[correctIdx]){
                 int temp=nums[correctIdx];
                 nums[correctIdx]=nums[i];
                 nums[i]=temp;
@@ -22,7 +22,7 @@ class Solution {
             return j+1;
           }
         }
-        return j+1;
+        return j+1;// when j cross nums array and answer is outside array
       
     }
 }
