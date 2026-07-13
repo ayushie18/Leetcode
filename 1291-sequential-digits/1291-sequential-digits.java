61809b0492ae5428992b531d04c9518ca7b046b5
ayushie18 <ayushi18parashar@gmail.com>
2026-07-13 16:59:39 +0530

    Time: 0 ms (100%), Space: 42.4 MB (36.36%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -16,21 +16,34 @@ class Solution {
         //         ans.add(i);
         //     }
         // }
-        String digits = "123456789";
-        for (int len = 2; len <= 9; len++) {
-            for (int i = 0; i + len <= 9; i++) {
-                int num = Integer.parseInt(
-                    digits.substring(i, i + len)
-                );
+        // String digits = "123456789";
+        // for (int len = 2; len <= 9; len++) {
+        //     for (int i = 0; i + len <= 9; i++) {
+        //         int num = Integer.parseInt(
+        //             digits.substring(i, i + len)
+        //         );
 
-                if (num >= low && num <= high) {
-                    ans.add(num);
-                }
-            }
+        //         if (num >= low && num <= high) {
+        //             ans.add(num);
+        //         }
+        //     }
             
-        }
-        Collections.sort(ans);
-        return ans;
-    
-    }
+        // }
+        // Collections.sort(ans);
+        // return ans;
+        for(int i=1;i<=9;i++){
+            int n=i;
+            int nextDig=i+1;
+            while(n<=high && nextDig<=9){
+              n=n*10+nextDig;
+              if(low<=n && n<=high){
+                ans.add(n);
+              }
+              nextDig++;
+            }
+        } 
+       Collections.sort(ans);  
+       return ans;
+
+   }
 }
