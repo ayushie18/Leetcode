83f114d62c02a1db961e5234fd0b515cbe1499e1
ayushie18 <ayushi18parashar@gmail.com>
2026-07-15 03:01:17 +0530

    Time: 14 ms (7.96%), Space: 47.8 MB (12.83%) - LeetHub

---
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countHomogenous(String s) {
+        int mod=1000000007;
+        int count=1;
+        int freq=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                freq++;
+            }
+            else{
+               freq=1;
+            }
+
+            count=(count+freq)%mod;
+        }
+
+        return count%mod;
+        
+    }
+}
