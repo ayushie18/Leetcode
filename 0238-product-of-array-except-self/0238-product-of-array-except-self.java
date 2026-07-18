cf82d98c455e2ca8e9e1e28c6124aedd5e656136
ayushie18 <ayushi18parashar@gmail.com>
2026-07-18 19:49:26 +0530

    Time: 2 ms (93.94%), Space: 72.4 MB (6.4%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,18 +6,19 @@ class Solution {
             prePro[i]=prePro[i-1]*nums[i-1];
 
         }
-        int[] sufPro=new int[nums.length];
-        sufPro[nums.length-1]=1;
+        // int[] sufPro=new int[nums.length];
+        // sufPro[nums.length-1]=1;
+        // for(int i=nums.length-2;i>=0;i--){
+        //     sufPro[i]=sufPro[i+1]*nums[i+1];
+        // }
+        int suf=1;
+       // int ans[]=new int[nums.length];
         for(int i=nums.length-2;i>=0;i--){
-            sufPro[i]=sufPro[i+1]*nums[i+1];
+            suf*=nums[i+1];
+            prePro[i]*=suf;
         }
 
-        int ans[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            ans[i]=prePro[i]*sufPro[i];
-        }
-
-        return ans;
+        return prePro;
         
 
         
