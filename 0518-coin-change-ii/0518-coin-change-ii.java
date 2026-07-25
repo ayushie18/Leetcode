f350615d7eda9aecca424a631fca8570b6fc9e5c
ayushie18 <ayushi18parashar@gmail.com>
2026-07-25 09:29:18 +0530

    Time: 17 ms (29.91%), Space: 56.6 MB (46.01%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -4,14 +4,18 @@ class Solution {
        // if(i==coins.length) return 0;
        if(i==0) return 0;
 
-        int take=0;
+        //int take=0;
         if(dp[i][amount]!=-1) return dp[i][amount];
         if(coins[i-1]<=amount){
-            take=TotalWays(i,amount-coins[i-1],coins,dp);
+            //take=TotalWays(i,amount-coins[i-1],coins,dp);
+            dp[i][amount]=TotalWays(i-1,amount,coins,dp)+TotalWays(i,amount-coins[i-1],coins,dp);
 
         }
-        int skip=TotalWays(i-1,amount,coins,dp);
-        return dp[i][amount]=take+skip;
+       // int skip=TotalWays(i-1,amount,coins,dp);
+       else{
+        dp[i][amount]=TotalWays(i-1,amount,coins,dp);
+       }
+        return dp[i][amount];
         
 
     }
