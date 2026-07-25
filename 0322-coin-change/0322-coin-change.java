4ac8f3347493ab3e272e7acaff5efeeb7ff6e059
ayushie18 <ayushi18parashar@gmail.com>
2026-07-25 11:41:31 +0530

    Time: 33 ms (16.17%), Space: 48.1 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,16 +1,16 @@
 class Solution {
     public int helper(int i,int[] coins,int amount,int[][]dp){
         if(amount==0) return 0;
-        if(i==coins.length) return (int)1e9;
+        if(i==0) return (int)1e9;
         if(dp[i][amount]!=-1){
             return dp[i][amount];
         }
-        if(coins[i]<=amount){
-            dp[i][amount]=Math.min((1+helper(i,coins,amount-coins[i],dp)),helper(i+1,coins,amount,dp));
+        if(coins[i-1]<=amount){
+            dp[i][amount]=Math.min((1+helper(i,coins,amount-coins[i-1],dp)),helper(i-1,coins,amount,dp));
 
         }
         else{
-            dp[i][amount]=helper(i+1,coins,amount,dp);
+            dp[i][amount]=helper(i-1,coins,amount,dp);
         }
         return dp[i][amount];
 
@@ -21,7 +21,7 @@ class Solution {
         for(int [] row:dp){
             Arrays.fill(row,-1);
         }
-        int count=helper(0,coins,amount,dp);
+        int count=helper(n,coins,amount,dp);
         if(count>=(int)1e9) return -1;
 
         return count;
