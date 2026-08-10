1c52c99fbcdbd51ee4798ea6515bc23b3671151d
ayushie18 <ayushi18parashar@gmail.com>
2026-08-11 00:15:13 +0530

    Time: 0 ms (100%), Space: 42 MB (66.3%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -16,10 +16,21 @@ class Solution {
     }
     public int uniquePaths(int m, int n) {
         dp=new int[m][n];
-        for(int []row:dp){
-        Arrays.fill(row,-1);
+        // for(int []row:dp){
+        // Arrays.fill(row,-1);
+        // }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                }
+            }
         }
-        return paths(0,0,m,n,dp);
+        return dp[m-1][n-1];
+       // return paths(0,0,m,n,dp);
         
     }
 }
