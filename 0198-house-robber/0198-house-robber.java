688fb7e775ab33166f2d3956ced5ecdbcdbd93b0
ayushie18 <ayushi18parashar@gmail.com>
2026-08-24 14:41:04 +0530

    Time: 0 ms (100%), Space: 43 MB (19.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,16 @@
 class Solution {
-  
-    public int amount(int [] nums, int idx,int []dp){
-        
-        if(idx<0) return 0;
-        if(idx==0) return nums[0];
-        if(dp[idx]!=-1) return dp[idx];
-
-        return dp[idx]=Math.max(amount(nums,idx-1,dp),(nums[idx]+amount(nums,idx-2,dp)));
-
-    }
     public int rob(int[] nums) {
-      int n=nums.length;
-      int[] dp=new int[n];
-      for(int i=0;i<n;i++){
-        dp[i]=-1;
-      }
-
-      return amount(nums,n-1,dp);// Backward recursion
+        int n=nums.length;
+        int[]dp=new int[n];
+        dp[0]=nums[0];
+        if(n>1){
+            dp[1]=Math.max(nums[0],nums[1]);
+        }
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+       
+        return dp[n-1];
         
     }
 }
