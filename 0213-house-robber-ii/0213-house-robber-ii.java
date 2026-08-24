48607ac37aa3ea6137372b4f75330f85f10b4721
ayushie18 <ayushi18parashar@gmail.com>
2026-08-24 14:56:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (41.9%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,27 +1,27 @@
 class Solution {
-     public int amount(int [] nums,int start, int last,int[]dp){
-        if(last<start) return 0;
-        if(last==start) return nums[start];
-        if(dp[last]!=-1) return dp[last];
+    public int amount(int s,int e,int[]nums,int[]dp){
+        if(s>e) return 0;
+        if(s==e) return nums[s];
+        if(dp[s]!=-1) return dp[s];
 
-        return dp[last]=Math.max(amount(nums,start,last-1,dp),(nums[last]+amount(nums,start,last-2,dp)));
+        int pick=nums[s]+amount(s+2,e,nums,dp);
+        int skip=amount(s+1,e,nums,dp);
 
+        return dp[s]=Math.max(pick,skip);
     }
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1) return nums[0];
-        int[] dp1=new int[n];
+        int[]dp1=new int[n];
         Arrays.fill(dp1,-1);
 
-        int[] dp2=new int[n+1];
+         
+        int[]dp2=new int[n];
         Arrays.fill(dp2,-1);
 
-        int choice1=amount(nums,0,n-2,dp1);
-        int choice2=amount(nums,1,n-1,dp2);
+        int choice1=amount(0,n-2,nums,dp1);
+        int choice2=amount(1,n-1,nums,dp2);
 
-
-    return Math.max(choice1,choice2);// Backward recursion
-        
+        return Math.max(choice1,choice2);
     }
-    
 }
