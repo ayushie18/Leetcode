bbbe033e4c7d947dc5d47ad6fa177aa806713cea
ayushie18 <ayushi18parashar@gmail.com>
2026-09-02 04:48:49 +0530

    Time: 2 ms (69.05%), Space: 45.3 MB (55.79%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -1,46 +1,27 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
+        int n=dungeon.length;
+        int m=dungeon[0].length;
 
-        int n = dungeon.length;
-        int m = dungeon[0].length;
+        int[][]dp=new int[n][m];
+        // last cell
+        dp[n-1][m-1]=Math.max(1,1-dungeon[n-1][m-1]);
 
-        int[][] dp = new int[n][m];
-
-        // Bottom-right cell
-        dp[n-1][m-1] = Math.max(1, 1 - dungeon[n-1][m-1]);
-
-        // Last row
-        for(int j = m-2; j >= 0; j--) {
-            dp[n-1][j] = Math.max(
-                1,
-                dp[n-1][j+1] - dungeon[n-1][j]
-            );
+        //last row
+        for(int j=m-2;j>=0;j--){
+            dp[n-1][j]=Math.max(1,dp[n-1][j+1]-dungeon[n-1][j]);
         }
-
-        // Last column
-        for(int i = n-2; i >= 0; i--) {
-            dp[i][m-1] = Math.max(
-                1,
-                dp[i+1][m-1] - dungeon[i][m-1]
-            );
+        // last col
+        for(int i=n-2;i>=0;i--){
+            dp[i][m-1]=Math.max(1,dp[i+1][m-1]-dungeon[i][m-1]);
         }
-
-        // Remaining cells
-        for(int i = n-2; i >= 0; i--) {
-            for(int j = m-2; j >= 0; j--) {
-
-                int next = Math.min(
-                    dp[i+1][j],
-                    dp[i][j+1]
-                );
-
-                dp[i][j] = Math.max(
-                    1,
-                    next - dungeon[i][j]
-                );
+        for(int i=n-2;i>=0;i--){
+            for(int j=m-2;j>=0;j--){
+                int next=Math.min(dp[i+1][j],dp[i][j+1]);
+                dp[i][j]=Math.max(1,next-dungeon[i][j]);
+             
             }
         }
-
-        return dp[0][0];
+       return dp[0][0]; 
     }
 }
