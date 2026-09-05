d2b6de3a755458e89ba8323cb58bc296cb62941b
ayushie18 <ayushi18parashar@gmail.com>
2026-09-06 00:16:21 +0530

    Time: 3 ms (92.97%), Space: 47.2 MB (9.32%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -10,14 +10,23 @@ class Solution {
             for(int i:nums){
                 curr.add(i);
             }
-            if(!ans.contains(curr)){
-               ans.add(curr);
-               return;
-            }
-           
-        }
+            // if(!ans.contains(curr)){// when we not use used hashset
+            //    ans.add(curr);
+            //    return;
+            // }
+            ans.add(curr);
+            return;
+        } 
+        
+         // Stores values already used at this level
+          HashSet<Integer> used = new HashSet<>();
 
         for(int i=idx;i<nums.length;i++){
+            // Skip duplicate choices at this level
+             if (used.contains(nums[i])){
+                 continue;
+             } 
+             used.add(nums[i]);
             swap(nums,i,idx);
             findPermutate(nums,idx+1,ans);
             swap(nums,i,idx);
