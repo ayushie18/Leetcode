723076b5f1f228d60658d18965b2f513f7de07db
ayushie18 <ayushi18parashar@gmail.com>
2026-09-21 02:02:47 +0530

    Time: 4 ms (5.05%), Space: 46.1 MB (5.32%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int reverseDegree(String s) {
+       //char [] arr={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+        HashMap<Character,Integer> map=new HashMap<>();
+        map.put('a',26);
+        map.put('b',25);
+        map.put('c',24);
+        map.put('d',23);
+        map.put('e',22);
+        map.put('f',21);
+        map.put('g',20);
+        map.put('h',19);
+        map.put('i',18);
+        map.put('j',17);
+        map.put('k',16);
+        map.put('l',15);
+        map.put('m',14);
+        map.put('n',13);
+        map.put('o',12);
+        map.put('p',11);
+        map.put('q',10);
+        map.put('r',9);
+        map.put('s',8);
+        map.put('t',7);
+        map.put('u',6);
+        map.put('v',5);
+        map.put('w',4);
+        map.put('x',3);
+        map.put('y',2);
+        map.put('z',1);
+
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            char a=s.charAt(i);
+            int val=map.get(a);
+            int curr=(i+1)*val;
+            ans+=curr;
+        }
+
+
+    return ans;    
+   }
+}
